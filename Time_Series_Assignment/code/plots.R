b1aadf91b1cd62c7695136bc50cbe5bf79980388
Jacques-rs <gerardrossouw@gmail.com>
2022-06-27 12:12:27 +0200

    Update plots.R
    add joint plot for the determinants of the real exchange rate to see whether there are any common trends; and/or whether there is a constant.

--- Time_Series_Assignment/code/plots.R
+++ Time_Series_Assignment/code/plots.R
@@ -34,12 +34,40 @@ plot_endog2 <- function(df){
         scale_color_hue(labels = c("Fiscal Balance",
                                        "Real Commodity\nPrices",
                                        "Relative Real\nGDP per Capita",
-                                       "Net Foreign Assets\nIncludin the\nForward Book",
+                                       "Net Foreign Assets\nincluding the\nForward Book",
                                        "Openness Indicator",
                                        "Relative Real\nInterest Rate")) +
         theme(legend.title = element_blank()) +
         scale_linetype(guide = "none") #+
         # ggthemes::theme_economist()
+
+    return(plot)
+}
+
+plot_endog2_joint <- function(df){
+
+    plot <- df %>%
+        mutate(across(names(.[,grepl(x = names(.),
+                                     pattern = "^L.*")]),
+                      function(x) x = exp(x)),
+               .keep = "unused") %>%
+        gather(Econ_Measure, Value, 2:13) %>%
+        dplyr::select(date, Econ_Measure, Value) %>%
+        filter(!Econ_Measure %in% c("LREERS", "LPR2GOLD", "LPR2COMM3",
+                                    "LPRCOMM3", "LPRCOMM5", "LPRGOLD")) %>%
+        ggplot() +
+        geom_line(aes(x = date, y = Value, linetype = Econ_Measure,
+                      color = Econ_Measure)) +
+        # facet_wrap(~Econ_Measure, scales = "free_y", ncol = 2) +
+        scale_color_hue(labels = c("Fiscal Balance",
+                                   "Real Commodity\nPrices",
+                                   "Relative Real\nGDP per Capita",
+                                   "Net Foreign Assets\nincluding the\nForward Book",
+                                   "Openness Indicator",
+                                   "Relative Real\nInterest Rate")) +
+        theme(legend.title = element_blank()) +
+        scale_color_discrete(guide = "none") #+
+    # ggthemes::theme_economist()
 
     return(plot)
 }
